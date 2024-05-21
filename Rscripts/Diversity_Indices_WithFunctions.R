9f4e30b3da09edcd04f0422ddc55d9173f884ed6
pleunipennings <pleuni@dds.nl>
2024-05-20 21:19:59 -0700

    Changed how we do the bootstrapping
    Now we take random samples from the entire population instead of just resampling the RES pop to make a new RES dataset and the SUS pop for a SUS dataset.
    To get the p-value we then determine how often the diff between the simulated RES diversity and the simulated SUS diversity is bigger than or equal to the real RES diversity and the real SUS diversity.

--- Rscripts/Diversity_Indices_WithFunctions.R
+++ Rscripts/Diversity_Indices_WithFunctions.R
@@ -1,13 +1,12 @@
 
+#######################
+####
+## Prepare to make calculate diversity indeces 
+####
+#######################
 
-#getwd()
-#setwd("/Users/Candace Clark/Dropbox/2017_CodeLab/BacterialResistanceProject_2020/2020_CandaceUTIpopgen/DataAnalysis")
-setwd("~/Dropbox/2017_CodeLab/BacterialResistanceProject_2020/2020_CandaceUTIpopgen/DataAnalysis")
+setwd("~/Documents/GitHub/Antibiotic_Resistance_Data_Analysis")
 library(dplyr)
-
-############ To calculate random Simpson's Index of Diversity (Difference in SID) ##############
-####################### SIMULATION ##########################
-#############################################################################
 
 #Write function to calculate SID Simpson Div Index
 CalcGiniSimpsonDivIndex<-function(array_of_counts){
@@ -32,42 +31,49 @@ CalcShannonIndex <- function(array_of_counts){
   return(Shannonvalue)
 }
 
-drugRfiles<-list.files("Data/", pattern="ST_Data.csv") #read the files with data
+#create list of all csv files we will read
+ResDocs<-list.files("Data/", pattern ="_ST_Data.csv")
+#Create empty dataframe to store data
 DivIndices<-data.frame("Dataset"= character(), "Pathogen" = character(), "Drug" = character(), "NumRes"= integer(), "NumSus"= integer(),  "SIvalueR" = double(), "SIvalueS" =  double(), "SIpvalue" = double() )
+#I want to remove a bunch of ST that are not really sequence types
+ListToRemove <- c("-", "Minor", "ND " ,  "OC39" , "U1"  ,  "U10"  , "U11",   "U12",   "U13" ,  "U14" ,  "U15" ,  "U2"  , 
+                  "U3"  ,  "U4" ,   "U5"  ,  "U6"  ,  "U7" ,   "U8"  ,  "U9")
 
-#for (g in 1:length(drugRfiles)){
-for (g in 1:length(drugRfiles)){
-    #g = 1
+#######################
+####
+## Loop through each dataset, and then for each dataset loop through all drugs 
+####
+#######################
+
+for (g in 1:length(ResDocs)){
   print(drugRfiles[g])
-  df<-read.csv(paste0("Data/",drugRfiles[g]), stringsAsFactors = F)#, row.names = 1
+  DataSet<-read.csv(paste0("Data/",drugRfiles[g]), stringsAsFactors = F)#, row.names = 1
+  ## List of ST to remove (because unclear if they are ST)
+  DataSet<-DataSet[which(!DataSet$ST %in% ListToRemove),] #May 2024 Pleuni removed bunch of ST categories. 
   #to change the name of antibiotics that are different in different datasets
-  df$Drug[df$Drug == "Trimethoprim-Sulfamethoxazole"]<- "Trim-Sulf"
-  df$Drug[df$Drug == "Trimethroprim_Sulfamethoxazole"]<- "Trim-Sulf"
-  df$Drug[df$Drug == "AmoxiClav"]<- "Amoxi-Clav"
-  df$Drug[df$Drug == "PipTaz"]<- "Pip-Taz"
-  df$Drug[df$Drug == "CefuroximeAxetil"]<- "Cefuroxime axetil"
-  df$Drug[df$Drug == "Tigecyclin"]<- "Tigecycline"
-  df$Drug[df$Drug == "Tetracyclin"]<- "Tetracycline"
-  df$Drug[df$Drug == "Penicillin G"]<- "Penicillin"
+  DataSet$Drug[DataSet$Drug == "Trimethoprim-Sulfamethoxazole"]<- "Trim-Sulf"
+  DataSet$Drug[DataSet$Drug == "Trimethroprim_Sulfamethoxazole"]<- "Trim-Sulf"
+  DataSet$Drug[DataSet$Drug == "AmoxiClav"]<- "Amoxi-Clav"
+  DataSet$Drug[DataSet$Drug == "PipTaz"]<- "Pip-Taz"
+  DataSet$Drug[DataSet$Drug == "CefuroximeAxetil"]<- "Cefuroxime axetil"
+  DataSet$Drug[DataSet$Drug == "Tigecyclin"]<- "Tigecycline"
+  DataSet$Drug[DataSet$Drug == "Tetracyclin"]<- "Tetracycline"
+  DataSet$Drug[DataSet$Drug == "Penicillin G"]<- "Penicillin"
   
-  #print(unique(df$ST))
-  df<-df[which(df$ST!="Minor" & df$ST != "ND " & df$ST != "-"),] #Oct 2021 Pleuni removed the minor category. 
-  #It is not one ST, but many, if we count it as one it messes up our diversity calculations). 
-  dname<-gsub("_ST_Data.csv",'',drugRfiles[g])
-  DrugList<-sort(unique(df$Drug))
+  dname<-gsub("_ST_Data.csv",'',drugRfiles[g]) #Get the study name
+  DrugList<-sort(unique(DataSet$Drug)) #get the list of drugs used in this study
   
-  sum_numRes<- df %>% group_by(Drug) %>% summarize(sum = sum(NumRes))
-  DrugList<-DrugList[which(sum_numRes$sum>0)]
+  sum_numRes<- DataSet %>% group_by(Drug) %>% summarize(sum = sum(NumRes))
+  DrugList<-DrugList[which(sum_numRes$sum>0)] #Keep only the drugs for which there is some resistance
   
   pdf(file = paste0("Output/DivIndices_Histograms/Simulated_Histogram_GSI_", dname,".pdf"), width = 8, height = 8) #width = 8. height = 2.5
   par(mfrow=c(2,3), oma=c(0,0,2,0))
   
   for (i in 1:length(DrugList)){ 
-    #  i=1
     #stepwise process to calculate observed SI value 
     drugName<-DrugList[i]
     #subset the data frame to a specific drug
-    df_for_drug<-df[df$Drug==drugName,]
+    df_for_drug<-DataSet[DataSet$Drug==drugName,]
     
     #Calculate SI values of Resistant population
     GSIvalueRealR = CalcGiniSimpsonDivIndex(df_for_drug$NumRes)
@@ -79,10 +85,10 @@ for (g in 1:length(drugRfiles)){
     InvSIvalueRealS = CalcInverseSimpsonIndex(df_for_drug$NumSusc)
     ShanvalueRealS = CalcShannonIndex(df_for_drug$NumSusc)
     
-    #create the data frame to perform random sampling
-    re.ST<- rep(df_for_drug$ST, times=df_for_drug$NumRes)
-    su.ST<-rep(df_for_drug$ST, times=df_for_drug$NumSusc)
-    
+    #create a list of all samples to perform random sampling from
+    #this list lists one by one the ST for the resistant samples and the ST for the susc samples. 
+    ListForSampling <- rep(df_for_drug$ST, times=(df_for_drug$NumRes + df_for_drug$NumSusc))
+
     GSIvaluesRES = c(); GSIvaluesSUS = c() 
     InvSIvaluesRES = c(); InvSIvaluesSUS = c()
     ShannonvaluesRES = c(); ShannonvaluesSUS = c()
@@ -91,13 +97,13 @@ for (g in 1:length(drugRfiles)){
     for(j in 1:1000)  {#added seed for reproducible data 
       set.seed(j)
       #Resistant population
-      drugR <-sample(re.ST, length(re.ST),replace = T) #collecting only randomized data from the oringinal data set
+      drugR <-sample(ListForSampling, sum(df_for_drug$NumRes),replace = T) #collecting only randomized data from the oringinal data set
       SimResultsR<-data.frame(table(drugR))
       GSIvaluesRES = c(GSIvaluesRES, CalcGiniSimpsonDivIndex(SimResultsR$Freq))
       InvSIvaluesRES = c(InvSIvaluesRES, CalcInverseSimpsonIndex(SimResultsR$Freq))
       ShannonvaluesRES = c(ShannonvaluesRES, CalcShannonIndex(SimResultsR$Freq))
       #Susceptible population
-      drugS <-sample(su.ST, length(su.ST),replace = T) #collecting only randomized data from the oringinal data set
+      drugS <-sample(ListForSampling, sum(df_for_drug$NumSusc),replace = T) #collecting only randomized data from the oringinal data set
       SimResultsS<-data.frame(table(drugS))
       GSIvaluesSUS = c(GSIvaluesSUS, CalcGiniSimpsonDivIndex(SimResultsS$Freq))
       InvSIvaluesSUS = c(InvSIvaluesSUS, CalcInverseSimpsonIndex(SimResultsS$Freq))
@@ -112,32 +118,32 @@ for (g in 1:length(drugRfiles)){
     differenceShannonSim = ShannonvaluesSUS - ShannonvaluesRES #Shannon from 1000 sims
     
     #to calculate P-value
-    pValueGSI = (length(which(differenceGSISim<0)))/1000 
-    pValueInvSI = (length(which(differenceInvSISim<0)))/1000 
-    pValueShannon = (length(which(differenceShannonSim<0)))/1000 
+    pValueGSI = (length(which(differenceGSISim>=differenceGSIReal)))/1000 
+    pValueInvSI = (length(which(differenceInvSISim>=differenceInvSIReal)))/1000 
+    pValueShannon = (length(which(differenceShannonSim>=differenceShannonReal)))/1000 
     
     #to make histogram
-    hist(differenceGSISim, xlim = range(c(differenceGSISim, -0.3,0.3)), ylim=c(0,300),breaks=20,
+    hist(differenceGSISim, xlim = range(c(differenceGSIReal,differenceGSISim, -0.3,0.3)), ylim=c(0,300),breaks=20,
          main = paste(drugName, "Gini-Simp Ind"),
          xlab = "GSI values")
     mtext("Bootstrapped difference in diversity Sus - Res", line=-0.1, side=3, outer=TRUE, cex=0.9)
-    mtext(paste0(df$Pathogen," , ", dname) ,side = 3, line = -0.999, outer=TRUE, cex=0.8)
-    lines(x = c(0,0), y = c(0,300), col = "blue")
+    mtext(paste0(DataSet$Pathogen," , ", dname) ,side = 3, line = -0.999, outer=TRUE, cex=0.8)
+    lines(x = c(differenceGSIReal,differenceGSIReal), y = c(0,350), col = "blue")
     text(0, 300, labels = paste0("p-value = ", pValueGSI),col = " dark green")
     
-    hist(differenceInvSISim, xlim = range(c(differenceInvSISim, -2,2)), 
-         ylim = c(0,300), main = paste(drugName, "InverseSI"))
-    lines(x = c(0,0), y = c(0,300), col = "blue")
+    hist(differenceInvSISim, xlim = range(c(differenceInvSIReal,differenceInvSISim, -2,2)), 
+         ylim = c(0,350), main = paste(drugName, "InverseSI"))
+    lines(x = c(differenceInvSIReal,differenceInvSIReal), y = c(0,300), col = "blue")
     text(0, 300, labels = paste0("p-value = ", pValueInvSI),col = " dark green")
     
-    hist(differenceShannonSim, xlim = range(c(differenceShannonSim, -2,2)), 
-         ylim = c(0,300), main = paste(drugName, "Shannon"))
-    lines(x = c(0,0), y = c(0,300), col = "blue")
+    hist(differenceShannonSim, xlim = range(c(differenceShannonReal,differenceShannonSim, -2,2)), 
+         ylim = c(0,350), main = paste(drugName, "Shannon"))
+    lines(x = c(differenceShannonReal,differenceShannonReal), y = c(0,300), col = "blue")
     text(0, 300, labels = paste0("p-value = ", pValueShannon),col = " dark green")
     
     #add info to DivIndices
     DivIndices<-rbind(DivIndices, 
-                        data.frame("Dataset"=dname,"Pathogen" = df$Pathogen[1], 
+                        data.frame("Dataset"=dname,"Pathogen" = DataSet$Pathogen[1], 
                                    "Drug" = drugName, "NumRes"= sum(df_for_drug$NumRes), 
                                    "NumSus"= sum(df_for_drug$NumSusc), 
                                    "GSIvalueR" = GSIvalueRealR, "GSIvalueS" = GSIvalueRealS, "GSIpvalue" = pValueGSI, 
@@ -148,10 +154,6 @@ for (g in 1:length(drugRfiles)){
   dev.off()
 }
 
-#to create a csv file with all the results created above and date in file name, and stores in "Old" folder 
-#file.copy(from=paste("Output/DivIndices",".csv", sep = ""), to=paste("Output/Old/Archived_DivIndices",Sys.Date(),".csv", sep = ""))
-
 #to create a csv file with all the results created above, and stores in "Output" folder
-#03/10/2022: Anjani added (sep = "") to remove space in the file name
 write.csv(x = DivIndices, file = paste("Output/DivIndices",".csv", sep = ""), row.names = FALSE)
 
